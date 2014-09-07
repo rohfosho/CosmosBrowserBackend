435412559aaf4b0456638a7921408993ea4e0efd
Rohith Varanasi <Rohvar98@gmail.com>
2014-09-07 10:05:01 -0400

    fuck yeah

--- routes/url.js
+++ routes/url.js
@@ -37,28 +37,54 @@ router.get('/', function(req, res) {
 router.post('/sms', function(req, res) {
   var resp = new twilio.TwimlResponse();
   var tURL = req.body.Body;
+  console.log(tURL);
 
-  request('http://facebook.com', function (error, response, body) {
+  request('http://stefanaleksic.com', function (error, response, body) {
     var $ = cheerio.load(body);
     $('img').remove();
     $('head').remove();
     $('script').remove();
     $('link').remove();
+    $('body').children().removeAttr('style');
+    $('body').children().removeAttr('class');
+    $('body').children().removeAttr('id');
+    $('body').children().removeAttr('name');
+    $('body').children().removeAttr('src');
+    $('body').children().removeAttr('href');
     msg = $('body').html()+'';
 
     zlib.gzip(msg, function(err, result) {
       if(err) {
         console.log('ERROR\t'+err);
       } else {
-        console.log('Message\t'+msg); 
-        console.log('Result\t'+result);
-        msg = result;
+        //console.log('Message\t'+msg); 
+        //console.log('Result\t'+result);
+        var temp = new Buffer(msg).toString('base64');
+        console.log(temp);
+        msg = temp;
+        sendIt(msg, resp, res);
       }
     });
   });
 
-  resp.message(msg);
-  res.send(resp.toString());
 });
+
+function sendIt(temp, resp, res){
+  var messages = new Array, slices = Math.ceil(temp.length/1594);
+  console.log('\n'+slices);
+  
+  for(var i=0;i<slices;i++){
+    console.log(temp.substring(i*1600,(i*1594+1594))+'\n');
+    messages.push(i+' '+temp.substring(i*1594,(i*1594+1594)));
+  }
+
+  console.log(messages.length);
+
+  for(var j=0;j<messages.length;j++){
+    resp.message(messages[j]);
+  }
+
+  res.send(resp.toString());
+}
 
 module.exports = router;
