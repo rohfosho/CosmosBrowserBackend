6c37bda3d2b46a888c70b8ec18ccd84f8793ed42
rohfosho <Rohvar98@gmail.com>
2014-09-21 21:48:52 -0400

    cut down text sizes to 160, also minified html

--- routes/url.js
+++ routes/url.js
@@ -1,66 +1,78 @@
-//I just spotted some minor things that could be cleaned up code-wise
-//I don't have the time to write tests for this today, so code may be non-functional
-
 var express = require('express'),
     twilio  = require('twilio'),
     router  = express.Router(),
     request = require('request'),
     cheerio = require('cheerio'),
-    zlib    = require('zlib');
+    zlib    = require('zlib'),
+    minify  = require('html-minifier').minify,
+    md      = require('html-md');
 
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-    'use strict';
+  'use strict';
   res.send('lolzard');
 });
 
 router.post('/sms', function(req, res) {
   'use strict';
   var resp = new twilio.TwimlResponse(),
-      tURL = req.body.Body,
-      msg = '';
-    
-    function sendIt(temp, resp, sendItRes){
-      var messages = new Array, slices = Math.ceil(temp.length/1594);
-      
-      for(var i=0;i<slices;i++){
-        messages.push(i+' '+temp.substring(i*1594,(i*1594+1594)));
-      }
-    
-      for(var j=0;j<messages.length;j++){
-        resp.message(messages[j]);
-      }
-    
-      res.send(resp.toString());
+  tURL = req.body.Body,
+  msg = '';
+
+console.log(tURL);
+
+request(tURL, function (error, response, body) {
+  var $ = cheerio.load(body);
+  $('img').remove();
+  $('head').remove();
+  $('script').remove();
+  $('link').remove();
+  $('body').children().removeAttr('style');
+  $('body').children().removeAttr('class');
+  $('body').children().removeAttr('id');
+  $('body').children().removeAttr('name');
+  $('body').children().removeAttr('src');
+  $('body').children().removeAttr('href');
+  //    $('*').each(function() {      // iterate over all elements
+  //        this[0].attribs = {};     // remove all attributes
+  //    });
+  msg = $('body').html();
+  var lol = md(msg); 
+
+  msg = '<html><body>'+msg+'</body></html>';
+  msg = minify(msg, {collapseWhitespace:true, removeComments: true});
+  console.log(msg);
+  zlib.gzip(msg, function(err, result) {
+    var messageToSend = new Buffer(msg).toString('base64');
+    console.log(messageToSend);
+
+    if(err) {
+      throw err;
     }
-
-  request(tURL, function (error, response, body) {
-    var $ = cheerio.load(body);
-    $('img').remove();
-    $('head').remove();
-    $('script').remove();
-    $('link').remove();
-    $('body').children().removeAttr('style');
-    $('body').children().removeAttr('class');
-    $('body').children().removeAttr('id');
-    $('body').children().removeAttr('name');
-    $('body').children().removeAttr('src');
-    $('body').children().removeAttr('href');
-//    $('*').each(function() {      // iterate over all elements
-//        this[0].attribs = {};     // remove all attributes
-//    });
-    msg = $('body').html()+'';
-
-    zlib.gzip(msg, function(err, result) {
-      var messageToSend = new Buffer(result).toString('base64');
-      
-      if(err) {
-        throw err;
-      }
-      sendIt(messageToSend, resp, res);
-     });
+    //console.log(messageToSend);
+    sendIt(messageToSend, resp, res);
   });
+});
+
+function sendIt(temp, resp, sendItRes){
+  var messages = new Array, slices = Math.ceil(temp.length/160);
+  
+  console.log("Slices:\t"+slices);
+
+  for(var i=0;i<slices;i++){
+    if(i == slices - 1)
+      messages.push(i+'%'+temp.substring(i*158,(i*158+158))+'%');
+    else
+      messages.push(i+'%'+temp.substring(i*159,(i*159+159)));
+  }
+
+  for(var j=0;j<messages.length;j++){
+    resp.message(messages[j]);
+  }
+
+  res.send(resp.toString());
+}
 });
 
 
